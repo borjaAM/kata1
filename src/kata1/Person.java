dbc3a9c07f66b2a1b5ec140ebf5dd609d3bed7ba
BorjaAM <borja.gallifrey@gmail.com>
2020-10-07 11:20:13 +0100

    Project created

---
+++ src/kata1/Person.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata1;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Person {
+    // final no permite modificar los valores de las variables
+    private final String name;
+    private final LocalDate birthdate;
+
+    public Person(String name, LocalDate birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+    
+    public int getAge(){
+        return (int) toYears(LocalDate.now().toEpochDay() - birthdate.toEpochDay());
+    }
+    
+    public long toYears(long l){
+        return l/365;
+    }
+}
